cf24c92915d4e87695fbedd69d305ac9807e0f8b
quamernasim <quamer23nasim@yahoo.com>
2023-02-17 14:56:07 +0530

    func dumped in preprocessing & plot_utils

---
+++ utils/plot_utils.py
@@ -0,0 +1,199 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def plot3Dlocation(rand_well):
+    """
+    Plots a 3D visualization of the well path for a given well.
+
+    Parameters:
+    -----------
+    rand_well : pandas.DataFrame
+        A DataFrame containing the data for a single well, including columns for X_LOC, Y_LOC, and Z_LOC.
+
+    Returns:
+    --------
+    None
+        The function displays the 3D visualization in a new window using matplotlib and does not return anything.
+    """
+
+    x_loc = rand_well.X_LOC
+    y_loc = rand_well.Y_LOC
+    z_loc = np.abs(rand_well.Z_LOC)
+
+    fig = plt.figure(figsize=(12, 12))
+    ax = plt.axes(projection='3d')
+    ax.plot3D(x_loc, y_loc, np.abs(z_loc))
+
+    ax.plot3D(x_loc.values[0], y_loc.values[0], z_loc.values[0], marker='s', color='black', ms=8)
+    ax.plot3D(x_loc.values[-1], y_loc.values[-1], z_loc.values[-1], marker='*', color='red', ms=8)
+    ax.set_xlabel('X Location')
+    ax.set_ylabel('Y Location')
+    ax.set_zlabel('TVD')
+    ax.invert_zaxis()
+    plt.show()
+
+def plot2Dlocation(rand_well):
+    """
+    Plots the X and Y locations, as well as the X and TVDss and Y and TVDss plots
+    of a random well using the provided data in a pandas DataFrame.
+
+    Parameters:
+    -----------
+    rand_well : pandas.DataFrame
+        The well data to plot. Must contain columns with X_LOC, Y_LOC, and Z_LOC.
+
+    Returns:
+    --------
+    None
+        The function displays the 2D visualization in a new window using matplotlib and does not return anything.
+    """
+
+    x_loc = rand_well.X_LOC
+    y_loc = rand_well.Y_LOC
+    z_loc = np.abs(rand_well.Z_LOC)
+
+    _, ax = plt.subplots(1, 3, figsize = (20, 5))
+
+    ax[0].plot(x_loc, y_loc)
+    ax[0].set_title('X Location vs Y Location')
+    ax[0].set_xticks([])
+    ax[0].set_yticks([])
+    ax[0].set_xlabel('X')
+    ax[0].set_ylabel('Y')
+
+    ax[1].plot(x_loc, z_loc)
+    ax[1].set_title('X Location vs TVDss')
+    ax[1].set_xticks([])
+    ax[1].set_xlabel('X')
+    ax[1].set_ylabel('TVDss')
+    ax[1].plot(x_loc.values[0], z_loc.values[0], marker='s', color='black', ms=8)
+    ax[1].plot(x_loc.values[-1], z_loc.values[-1], marker='*', color='red', ms=8)
+    ax[2].invert_yaxis()
+
+    ax[2].plot(y_loc, z_loc)
+    ax[2].set_title('Y Location vs TVDss')
+    ax[2].set_xticks([])
+    ax[2].set_xlabel('Y')
+    ax[2].set_ylabel('TVDss')
+    ax[2].plot(y_loc.values[0], z_loc.values[0], marker='s', color='black', ms=8)
+    ax[2].plot(y_loc.values[-1], z_loc.values[-1], marker='*', color='red', ms=8)
+    ax[2].invert_yaxis()
+    plt.show()
+
+def xaxis(ax, xmin, xmax, spines_pos, color, log_name, name_size, y):
+    """
+    Set the properties of the x-axis of a matplotlib plot.
+    Parameters:
+    -----------
+    ax : matplotlib.axes.Axes object
+        The axes object on which to set the x-axis properties.
+    xmin : float
+        The minimum value to be shown on the x-axis.
+    xmax : float
+        The maximum value to be shown on the x-axis.
+    spines_pos : float
+        The position of the x-axis spines (in points) relative to the plot.
+    color : str
+        The color of the x-axis and its labels.
+    log_name : str
+        The name of the x-axis label.
+    name_size : int
+        The size of the x-axis label.
+    y : float
+        The y-coordinate of the x-axis label.
+
+    Returns:
+    --------
+    None
+    """
+    ax.set_xticks((xmin, xmax))
+    ax.spines['top'].set_position(('outward', spines_pos))
+    ax.spines['top'].set_color(color)
+    ax.xaxis.set_ticks_position('top')
+    ax.xaxis.set_label_position('top')
+    ax.xaxis.set_label_coords(0.5, y)
+    ax.set_xlabel(log_name, color = color, size = name_size)
+    ax.tick_params(axis = 'x', colors = color, size = 0, labelsize = 10)
+
+def log_plot(logs):
+    """
+    Plot well log data using a five-panel plot with shared y-axis.
+
+    Parameters:
+    -----------
+    logs : pandas.DataFrame
+        DataFrame containing well log data. It should have columns for depth
+        (DEPTH_MD) and at least the following logs: CALI, GR, SP, RSHA, RMED,
+        RDEP, RHOB, NPHI, and DTC. Other columns will be ignored.
+
+    Returns:
+    --------
+    None
+    """
+    
+    _, ax = plt.subplots(1,5, figsize = (10, 15), sharey = True)
+    ax[0].invert_yaxis()
+    ax[0].grid()
+
+    ax_gr_wrap = ax[0].twiny()
+    ax_sp_wrap = ax[0].twiny()
+
+    ax_rm_wrap = ax[1].twiny()
+    ax_rd_wrap = ax[1].twiny()
+
+    ax_nphi_wrap = ax[2].twiny()
+
+    ax[0].plot(logs.CALI, list(range(logs.DEPTH_MD.shape[0])), 'b')
+    ax_gr_wrap.plot(logs.GR, list(range(logs.DEPTH_MD.shape[0])), 'r')
+    ax_sp_wrap.plot(logs.SP, list(range(logs.DEPTH_MD.shape[0])), 'g' )
+
+    ax[1].plot(logs.RSHA, list(range(logs.DEPTH_MD.shape[0])), 'b')
+    ax_rm_wrap.plot(logs.RMED, list(range(logs.DEPTH_MD.shape[0])), 'r')
+    ax_rd_wrap.plot(logs.RDEP, list(range(logs.DEPTH_MD.shape[0])), 'g')
+
+    ax[2].plot(logs.RHOB, list(range(logs.DEPTH_MD.shape[0])), 'b')
+    ax_nphi_wrap.plot(logs.NPHI, list(range(logs.DEPTH_MD.shape[0])), 'r')
+
+    ax[3].plot(logs.DTC, list(range(logs.DEPTH_MD.shape[0])), 'b')
+
+    ax[0].set_xlim(6, 24)
+    ax_gr_wrap.set_xlim(0, 150)
+    ax_sp_wrap.set_xlim(-150, 150)
+
+    ax[1].set_xlim(0.2, 200)
+    ax_rm_wrap.set_xlim(0.2, 200)
+    ax_rd_wrap.set_xlim(0.2, 200)
+
+    ax[1].set_xscale('log')
+    #ax_rm_wrap.set_xscale('log')
+    #ax_rd_wrap.set_xscale('log')
+
+    ax[2].set_xlim(0.95, 2.95)
+    ax_nphi_wrap.set_xlim(-0.15, 1.05)
+    ax_nphi_wrap.invert_xaxis()
+
+    ax[3].set_xlim(40, 240)
+    ax[3].invert_xaxis()
+
+    logs_group_temp = logs.GROUP.fillna('N/A')
+    encoder_temp = LabelEncoder()
+    encoder_temp.fit(logs_group_temp)
+    logs_group_temp_encoded = pd.Series(encoder_temp.transform(logs_group_temp))
+    cl = np.repeat(np.expand_dims(logs_group_temp_encoded.values,1), 200, 1)
+    ax[4].imshow(cl, interpolation='none', aspect='auto')
+
+
+    ax[4].set_xticklabels([])
+
+    xaxis(ax[0], 6, 24, 0, 'b', 'CALI', 10, 1.003)
+    xaxis(ax_gr_wrap, 0, 150, 25, 'r', 'GR', 10, 1.029)
+    xaxis(ax_sp_wrap, -150, 150, 50, 'g', 'SP', 10, 1.051)
+
+    xaxis(ax[1], 0, 200, 0, 'b', 'Rs', 10, 1.003)
+    xaxis(ax_rm_wrap, 0.2, 200, 25, 'r', 'Rm', 10, 1.029)
+    xaxis(ax_rd_wrap, 0.2, 200, 50, 'g', 'Rd', 10, 1.051)
+
+    xaxis(ax[2], 0.95, 2.95, 0, 'b', 'RHOB', 10, 1.003)
+    xaxis(ax_nphi_wrap, -0.15, 1.05, 25, 'r', 'NPHI', 10, 1.029)
+
+    xaxis(ax[3], 40, 240, 0, 'b', 'Sonic', 10, 1.003)
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -168,3 +168,148 @@ def remove_column_with_half_of_nan_value(data):
     
     print("Removing columns which have 50% of missing data i.e., 'SGR', 'ROP', 'DTS', 'DCAL', 'MUDWEIGHT', 'RMIC', 'ROPA', 'RXO', 'BS', 'DRHO'")
     data.drop(['SGR', 'ROP', 'DTS', 'DCAL', 'MUDWEIGHT', 'RMIC', 'ROPA', 'RXO', 'BS', 'DRHO'], axis = 1, inplace = True)
+
+def get_nonoverlapping_groups(train_group, test_group):
+    """
+    Returns the names of the groups that appear in the testing data but not in the training data.
+
+    Parameters:
+    -----------
+    train_group : pandas.Index
+        A pandas Index object containing the names of the unique groups in the training data.
+
+    test_group : pandas.Index
+        A pandas Index object containing the names of the unique groups in the testing data.
+
+    Returns:
+    --------
+    tuple
+        A tuple containing two elements:
+            - the number of groups in the testing data that do not appear in the training data
+            - a list of the names of those groups
+    """
+
+    count_nonoverlapping_groups = 0
+    nonoverlapped_groups = []
+    for group in test_group:
+        if (group in train_group) == False:
+            count_nonoverlapping_groups+=1
+            nonoverlapped_groups.append(group)
+    if count_nonoverlapping_groups !=0:
+        print("Total numbers of GROUPS in testing wells that is not overlapping in training wells: {} and their names are {}".format(count_nonoverlapping_groups, nonoverlapped_groups))
+        return (count_nonoverlapping_groups, nonoverlapped_groups)
+    else:
+        print("Total numbers of GROUPS in testing wells that is not overlapping in training wells: {}".format(count_nonoverlapping_groups))
+        return (count_nonoverlapping_groups, None)
+
+def missing_group_info(data):
+    """
+    Prints the number of data points with missing group information in the given DataFrame.
+
+    Parameters:
+    -----------
+    data : pandas.DataFrame
+        The DataFrame containing the data to be checked for missing group information.
+
+    Returns:
+    --------
+    None
+    """
+    
+    print('Total data points with missing Group information: {}'.format(data.GROUP.isna().sum()))
+
+def remove_formation_column(data):
+    """
+    Removes the 'FORMATION' column from the given DataFrame.
+
+    Parameters:
+    -----------
+    data : pandas.DataFrame
+        The DataFrame from which to remove the 'FORMATION' column.
+
+    Returns:
+    --------
+    None
+    """
+    
+    print('Removing FORMATION column from the dataset')
+    data.drop(['FORMATION'], axis = 1, inplace = True)
+
+def get_undeviated_well_info(data):
+    """
+    Returns a list of names of undeviated wells in the given DataFrame.
+
+    An undeviated well is defined as a well where all data points have the same X and Y coordinates.
+
+    Parameters:
+    -----------
+    data : pandas.DataFrame
+        The DataFrame from which to extract undeviated well names.
+
+    Returns:
+    --------
+    well_names : list of str
+        A list of names of undeviated wells in the input DataFrame.
+    """
+    
+    well_names = []
+    for well_name in preprocessing.get_well_names(data):
+        well_data = data[data.WELL == well_name]
+        if (well_data.X_LOC.value_counts().shape[0] == 1) and (well_data.Y_LOC.value_counts().shape[0] == 1):
+            well_names.append(well_name)
+            
+    print('Total Number of undeviated wells present in the dataset is {} and their names are:\n{}'.format(len(well_names), 
+                                                                                                          well_names))
+    return well_names
+
+def get_well_with_no_group_info(data, total_num_wells, well_names):
+    """
+    Returns a list of well names that have missing information on the 'GROUP' column in the input dataset.
+
+    Parameters:
+    -----------
+    data : pandas.DataFrame
+        The input dataset containing well information.
+    total_num_wells : int
+        The total number of wells in the dataset.
+    well_names : list of str
+        A list of well names in the dataset.
+
+    Returns:
+    --------
+    list of str
+        A list of well names that have missing information on the 'GROUP' column in the input dataset.
+    """
+    
+    well_with_missing_group_info = []
+
+    for well_names_index in range(total_num_wells):
+        single_well_name = well_names[well_names_index]
+        single_well_data = data[data['WELL'] == single_well_name]
+        total_group_data_missing = single_well_data.GROUP.isna().sum()
+        if total_group_data_missing != 0:
+            print('Well "{0}" is having missing information on GROUP and total data points missing this information is {1}'.format(single_well_name, total_group_data_missing))
+            well_with_missing_group_info.append(single_well_name)
+    print('\nTotal {} Wells have missing group information!'.format(len(well_with_missing_group_info)))
+    return well_with_missing_group_info
+
+def fill_group_na_value(data, well_name, method = 'bfill'):
+    """
+    Fills the missing GROUP values for the given well in the DataFrame.
+
+    Parameters:
+    -----------
+    data : pandas.DataFrame
+        The DataFrame containing the well data.
+    well_name : str
+        The name of the well to fill missing GROUP values for.
+    method : str, optional (default 'bfill')
+        The method used to fill missing values. It can be one of {'backfill', 'bfill', 'ffill', 'pad', 'nearest'}.
+
+    Returns:
+    --------
+    pandas.Series
+        The Series containing the filled GROUP values for the given well.
+    """
+    
+    return data[data['WELL'] == well_name].GROUP.fillna(method = method)
